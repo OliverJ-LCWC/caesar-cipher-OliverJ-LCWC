7ccd26f393490e619aef97937265d64abe099e82
OliverJ-LCWC <144791858+OliverJ-LCWC@users.noreply.github.com>
2024-10-14 12:44:51 +0000

    woo

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -1,10 +1,21 @@
-def encrypt(text, shift, letter_bank):
-    letter_bank = abcdefghijklnopqrstuvwxyz
-    encrypted_text = int(input("Enter the text you wish to encrypt: "))
-    for i in text.upper():
+def encrypt(text, shift):
+    letter_bank = "abcdefghijklnopqrstuvwxyz"
+    encrypted_text = ""
+    for i in text.lower():
         if i in letter_bank:
             index = (letter_bank.index(i) + shift) % len(letter_bank)
             encrypted_text += letter_bank[index]
         else:
             encrypted_text += i
-    return encrypted_text 
+    return encrypted_text
+
+def unencrypt(text, shift):
+    letter_bank = "abcdefghijklnopqrstuvwxyz"
+    encrypted_text = ""
+    for i in text.lower():
+        if i in letter_bank:
+            index = (letter_bank.index(i) - shift) % len(letter_bank)
+            encrypted_text += letter_bank[index]
+        else:
+            encrypted_text += i
+    return encrypted_text
